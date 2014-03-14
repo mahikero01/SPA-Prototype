89bd28d43aeee6995de8004cadf320b67222b482
mahikero01 <federicoparassarmiento@yahoo.com>
2014-03-15 03:42:58 +0800

    Up Files

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -22,60 +22,67 @@ spa.shell = ( function () {
 				+ '<div class="spa-shell-foot"> </div>'
 				+ '<div class="spa-shell-chat"> </div>'
 				+ '<div class="spa-shell-modal"> </div>',
-      chat_extend_time : 1000,
-      chat_retract_time : 300,
-      chat_extend_height : 450,
-      chat_retract_height : 15,
-      chat_extended_title : 'Click to retract',
-      chat_retracted_title : 'Click to extend'
+      		chat_extend_time : 1000,
+      		chat_retract_time : 300,
+      		chat_extend_height : 450,
+      		chat_retract_height : 15,
+      		chat_extended_title : 'Click to retract',
+      		chat_retracted_title : 'Click to extend'
 		},
 		stateMap = {
 			$container : null,
-     is_chat_retracted : true
+     		is_chat_retracted : true
 		},
 		jqueryMap = {
 		},
 		setJqueryMap,
-    toggleChat,
-    onClickChat,
+    	toggleChat,
+    	onClickChat,
 		initModule;
 		
 		//DOM methods
 		setJqueryMap = function () {
 			var $container = stateMap.$container;
 			jqueryMap = { 
-       $container : $container,
-      $chat : $container.find( '.spa-shell-chat' )
-     };
+       			$container : $container,
+      			$chat : $container.find( '.spa-shell-chat' )
+     		};
 		};
 		
-    toggleChat = function( do_extend, callback ) {
-        var
-           px_chat_ht = jqueryMap.$chat.height(),
-           is_open = px_chat_ht === configMap.chat_extend_height,
-           is_closed = px_chat_ht === configMap.chat_retract_height,
-           is_sliding = ! is_open && ! is_closed;
-       
-       if ( is_sliding ) { return false; }    
+    	toggleChat = function( do_extend, callback) {
+        	var
+           		px_chat_ht = jqueryMap.$chat.height(),
+           		is_open = px_chat_ht === configMap.chat_extend_height,
+           		is_closed = px_chat_ht === configMap.chat_retract_height,
+           		is_sliding = ! is_open && ! is_closed;
+       	
+       		if ( is_sliding ) { return false; };    
   
-       if ( do_extend ) {
-           jqueryMap.$chat.animate(
-              { height : configMap.chat_extend_height },
-              configMap.chat_extend_time,
-              function() {
-                 jqueryMap.$chat.attr(
-                    'title', configMap.chat_extended_title
-                 );
-                 stateMap.is_chat_retracted = false;
-                  if ( callback ) {
-                     callback ( jqueryMap.$chat );
-                  }
-              }
-           );
+      		 if ( do_extend ) {
+         	  jqueryMap.$chat.animate(
+              	{ height : configMap.chat_extend_height },
+              	configMap.chat_extend_time,
+              	function() {
+                 	jqueryMap.$chat.attr(
+                    	'title', configMap.chat_extended_title
+                 	);
+                 	stateMap.is_chat_retracted = false;
+                  	if ( callback ) {
+                     	callback ( jqueryMap.$chat );
+                  	};
+              	}
+           	)
            
-           return true;
-       };
+           		return true; 
+       }
     
+       
+       
+       
+       
+       
+       
+       
        jqueryMap.$chat.animate(
               { height : configMap.chat_retract_height },
               configMap.chat_retract_time,
@@ -86,7 +93,7 @@ spa.shell = ( function () {
                  stateMap.is_chat_retracted = true;
                   if ( callback ) {
                      callback ( jqueryMap.$chat );
-                  }
+                  };
               }
        );
     
