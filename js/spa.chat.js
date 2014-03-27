de0644ebde0962f7d1a38501ca3a3420ac730e42
Rico - Desktop <federicoparassarmiento@yahoo.com>
2014-03-27 19:06:28 +0800

    Up

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -4,31 +4,87 @@ newcap : true, 	nomen : true, plusplus : true,
 regexp : true,	sloppy : true, vars : false,
 white : true
 */
-/*global $, spa*/
+/*global $, spa, getComputedStyle */
 
 spa.chat = (function () {
 //module scope variable
 	var
 		configMap = {
 			main_html : String()
-				+ '<div style="padding:1em; color:#fff;">'
-				+ 'Say hello to chat'
+				+ '<div class="spa-chat">'
+					+ '<div class="spa-chat-head">'
+						+ '<div class="spa-chat-head-toggle">+</div>'
+						+ '<div class="spa-chat-head-title">chat</div>'
+					+ '</div>'
+					+ '<div class="spa-chat-closer">'
+					+ '<div class="spa-chat-sizer">'
+						+ '<div class="spa-chat-msgs"></div>'
+						+ '<div class="spa-chat-box">'
+							+ '<input type="text" />'
+							+ '<div>send</div>'
+						+ '</div>'
+					+ '</div>'
 				+ '</div>',
-			settable_map : {} 
+					
+			settable_map : {
+				slider_open_time: true,
+				slider_close_time: true,
+				slider_opened_em: true,
+				slider_closed_em: true,
+				slider_opened_title: true,
+				slider_closed_title: true,
+				
+				chat_model: true,
+				people_model: true,
+				set_chat_anchor: true
+			},
+			
+			slider_open_time: 250,
+			slider_close_time: 250,
+			slider_opened_em: 16, 
+			slider_closed_em: 2,
+			slider_opened_title: 'click to close', 
+			slider_closed_title: 'click to open', 
+			
+			chat_model: null,
+			people_model: null,
+			set_chat_anchor: null		
 		},
 		stateMap = {
-			$container : null
+     $append_target: null,
+     position_type: 'closed',
+     px_per_em: 0,
+     slider_hidden_px: 0,
+     slider_closed_px: 0,
+     slider_opened_px: 0
 		},
 		jqueryMap = {},
 		setJqueryMap,
+    getEmSize,
+    setPxSizes,
+    setSliderPosition,
+    onClickToggle,
 		configModule,
 		initModule;
 	
+ //utility
+ getEmSize = function ( elem ) {
+    return Number(
+       getComputedStyle( elem, '' ).fontSize.match(/\d*\.?\d*/) [0]
+    );
+ };
+ 
 	//DOM methods
 	setJqueryMap = function () {
 		var
-			$container = stateMap.$container;
-			jqueryMap = { $container : $container };
+     $append_target = stateMap.$append_target,
+     $slider = $append_target.find( '.spa-chat' );
+	
+			jqueryMap = { 
+        $slider: $slider,
+        $head: $slider.find( '.spa-chat-head' ),
+        $toggle: $slider.find( '.spa-chat-head-toggle' ),
+     };
 	};
 
 	//public methods
