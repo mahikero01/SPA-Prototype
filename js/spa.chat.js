612334b28fef9f870290d671448fb37bb2f79838
Rico - Laptop <federicoparassarmiento@yahoo.com>
2014-04-02 20:04:50 +0800

    Up

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -94,11 +94,18 @@ spa.chat = (function () {
   	};
   
   	setPxSizes = function () {
-    	var px_per_em, opened_height_em;
+    	var px_per_em, window_height_em, opened_height_em;
 
     	px_per_em = getEmSize( jqueryMap.$slider.get(0) );
+    	window_height_em = Math.floor(
+    		( $(window).height() / px_per_em ) + 0.5
+    	);
+    	
 
-    	opened_height_em = configMap.slider_opened_em;
+    	opened_height_em 
+    		= window_height_em > configMap.window_height_min_em
+    		? configMap.slider_opened_em
+    		: configMap.slider_opened_min_em;
 
     	stateMap.px_per_em        = px_per_em;
     	stateMap.slider_closed_px = configMap.slider_closed_em * px_per_em;
@@ -209,6 +216,17 @@ spa.chat = (function () {
     	configMap.set_chat_anchor = null;
     	
     	return true;
+  	};
+  
+  	handleResize = function () {
+  		if ( ! jqueryMap.$slider ) { return false; }
+  		
+  		setPxSizes();
+  		if ( stateMap.position_type === 'opened' ) {
+  			jqueryMap.$slider.css({ height : stateMap.slider_opened_px });
+  		}
+  		
+  		return true;
   	};
   
   	return {
