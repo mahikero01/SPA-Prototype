d53ed42a074d8a12c003a8e4865c707761d70dc6
Rico - Desktop <federicoparassarmiento@yahoo.com>
2014-04-04 14:29:36 +0800

    Up

---
+++ js/spa.util_b.js
@@ -0,0 +1,35 @@
+/*jslint 		browser : true, continue : true,
+devel : true, 	indent : 2, maxerr : 50,
+newcap : true, 	nomen : true, plusplus : true,
+regexp : true,	sloppy : true, vars : false,
+white : true
+*/
+/*global $, spa*/
+
+
+spa.util_b = (function () { 
+	'use strict';
+
+	//scope var
+	var
+		configMap = {
+			regex_encode_html : /[&"'><]/g,
+			regex_encode_noamp : /["'><]/g,
+			html_encode_map : {
+				'&' : '&#38;',
+				'"' : '&#34;',
+				"'" : '&#39;',
+				'>' : '&#62;',
+				'<' : '&#60;'
+			}
+		},
+		decodeHtml,
+		encodeHtml,
+		getEmSize;
+		
+	configMap.encode_noamp_map = $.extend(
+		{}, configMap.html_encode_map
+	);
+	delete configMap.encode_noamp_map['&'];
+	return{}; 
+}());
